e0535596aac5730f3cff3d82684a27680e6f60e8
jaeyoon492 <audwodbs492@gmailcom>
2024-08-28 12:51:17 +0900

    first commit

---
+++ detect.py
@@ -0,0 +1,25 @@
+from ultralytics import YOLO
+import cv2
+import matplotlib.pyplot as plt
+
+# 옵션 설정
+weights_path = 'runs/detect/train8/weights/best.pt'  # 학습된 모델의 경로
+source_path = 'images/click3.jpg'  # 검출할 이미지 또는 비디오 파일 경로
+
+model = YOLO(weights_path)
+
+# 이미지 로드
+image = cv2.imread(source_path)
+
+# 추론 수행
+confidence_threshold = 0.5 # 임계값 설정 (예: 0.5)
+results = model.predict(source=image, imgsz=max(image.shape[:2]), conf=confidence_threshold)
+
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+result_image = results[0].plot()  # 첫 번째 결과의 시각화된 이미지
+
+plt.imshow(result_image)
+plt.axis('off')
+plt.show()
+
+
---
+++ train.py
@@ -0,0 +1,15 @@
+from ultralytics import YOLO
+
+def train_model():
+    # 모델 생성 (YOLOv8n - 가장 작은 모델을 사용)
+    model = YOLO("yolov8m.pt")  # 또는 yolov8s.pt, yolov8m.pt, yolov8l.pt, yolov8x.pt
+
+    # 모델 학습
+    model.train(data="dataset/dataset.yaml", epochs=100, imgsz=640)
+    # metrics = model.val()
+    #
+    # results = model.predict(source="dataset/valid/images/0_background_Blitzcrank_png_jpg.rf.16a02129f99fb5aebce08235d7dd6dc4.jpg", save=True)  # 결과를 이미지로 저장
+    model.export(format="onnx")  # ONNX 형식으로 내보내기
+
+if __name__ == '__main__':
+    train_model()
